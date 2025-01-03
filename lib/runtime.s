f20698c6416d818784a47a177498e4e72fa8cd10
v420v <ibuki420v@gmail.com>
2025-01-03 23:59:22 +0900

    add: rewrite memory allocator in ibu close #17

--- lib/runtime.s
+++ lib/runtime.s
@@ -1,11 +1,3 @@
-.set HEAP_CAPACITY, 436870912 # 0.5GB
-
-# Allocator
-.bss
-heap:
-	.zero	HEAP_CAPACITY
-heap_size:
-	.zero	8
 
 .text
 
@@ -25,34 +17,3 @@ syscall:
 	syscall
 	leave
 	ret
-
-# func alloc(size i32) *u8;
-# size   16(rbp)
-.global	alloc
-alloc:
-	pushq	%rbp
-	movq	%rsp, %rbp
-	movq	16(%rbp), %rax
-	movq    %rax, -24(%rbp)
-	movq	heap_size(%rip), %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	cmpq	$HEAP_CAPACITY-1, %rax
-	jbe	.L.alloc.2
-	movl	$0, %eax
-	jmp	.L.alloc_ret_null
-.L.alloc.2:
-	movq	heap_size(%rip), %rax
-	leaq	heap(%rip), %rdx
-	addq	%rdx, %rax
-	movq	%rax, -8(%rbp)
-	movq	heap_size(%rip), %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movq	%rax, heap_size(%rip)
-	movq	-8(%rbp), %rax
-.L.alloc_ret_null:
-	popq	%rbp
-	ret
-
-
